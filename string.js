e19c446ea0ce6775f2c982dc995185c0e98bd7bb
shaikh <shaikhsafvan0305@gmail.com>
2023-09-09 16:25:37 +0530

    String Example 2

---
+++ string.js
@@ -0,0 +1,14 @@
+var favshow = "Game of Thrones";
+console.log(favshow.length)
+
+console.log(favshow[0]);
+
+console.log(favshow[favshow.length-1])
+
+console.log(favshow.indexOf("of"))
+
+console.log(favshow.slice(5,7))
+
+var srk = "My name is shaikh";
+console.log(srk.toUpperCase());
+console.log(srk.toLowerCase());

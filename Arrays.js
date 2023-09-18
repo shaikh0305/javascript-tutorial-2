6fc1125a95bdbda8e4d5c6b324a1c556398fa06a
shaikh <shaikhsafvan0305@gmail.com>
2023-09-18 20:35:24 +0530

    Operators

--- Arrays.js
+++ Arrays.js
@@ -30,7 +30,7 @@ const names2 =  names.lastIndexOf('Xiaomi')
 // names.includes('Apple')
 const names3 = names.includes('Huawei')
 
-console.log() 
+// console.log(names3) 
 
 
 
@@ -39,19 +39,19 @@ let names02 = ['Xiaomi','Huawei','Redmi']
 // Concat
 let names03 = names01.concat(names02)
 // Slick
-let names04 = names03.slice(2)
+let names04 = names03.slice()
 
-console.log() 
+// console.log(names04) 
 
 // For Loop
 for(let i = 0;i<names.length;i++){
-    // console.log(names[i])
+    // console.log(names[i],i)
 }
 
 // For of Loop
 for(let name of names){
     // console.log(name)
-}           
+}                       
 
 // For-Each
 names.forEach(function(name,index){
@@ -60,9 +60,9 @@ names.forEach(function(name,index){
 
 // Join
 let student = ['S','h','a','i','k','h']
-// console.log(student.join(''))
+// console.log(student.join('_'))
 
 // Split
-let student1 = ['S','h','a','i','k','h']
+let student1 = ['How are you doing today']
 // console.log(student1.splice(''))
 
---
+++ app.js
@@ -0,0 +1,57 @@
+/* Arithmetic Operators */
+
+// var a = 20;
+// var b = 10;
+// var c = a+b;
+// document.write(c)
+
+
+/* Relational or Comparison Operator */
+
+// var a = 20;
+// var b = 10;
+// var c = a>b;
+// document.write(c)
+
+
+/* Logical Operators */
+/* Logical And */
+
+// var a = 20>10;
+// var b = 10<20;
+// var c = a && b;
+// document.write(c)
+
+
+/* Logical OR*/
+
+// var a = 20<10;
+// var b = 10>20;
+// var c = a || b;
+// document.write(c)
+
+/* Logical Not */
+
+// var a = 20>10;
+// var c = !a;
+// document.write(c)
+
+
+/* Bitwise Operators */
+/* Bitwise Logical AND & */
+
+// var a = 5 ;
+// var b = 6 ; 
+// var c = a & b;
+// document.write(c);
+
+
+/* Bitwise Logical OR | */
+
+// var a = 5 ;
+// var b = 6 ; 
+// var c = a | b;
+// document.write(c);
+
+
+

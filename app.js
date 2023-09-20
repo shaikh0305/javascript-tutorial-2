9970b9604ebc423d6606a5715d1c8641b411bd10
shaikh <shaikhsafvan0305@gmail.com>
2023-09-20 19:48:30 +0530

    Switch Statement

--- app.js
+++ app.js
@@ -34,7 +34,7 @@
 
 // var a = 20>10;
 // var c = !a;
-// document.write(c)
+// document.write(c)    
 
 
 /* Bitwise Operators */
@@ -54,4 +54,141 @@
 // document.write(c);
 
 
+/* Assignment Operators */
 
+// var a = 100;
+// var b = a + 20;
+// document.write(b)
+
+
+/*-----------------*/
+
+
+/* Prompt Method  */
+
+// var a = prompt ("Enter Your Name :")
+// document.write(a);
+
+
+
+/* If Statement */
+
+// var a = 10;
+// var b = 20;
+// if ((a == 10) && (b == 20))
+// document.write("Rahul")
+
+/* Ex: 2 */
+
+// var a = prompt("Enter Your Roll : ");
+// if((a == 10))
+// document.write("Name : Rahul");
+
+
+
+/* if else statement */
+
+// var a = 10;
+// if(a ==10)
+// document.write("Name : Rahul");
+// else
+// document.write("Wrong Value");
+
+/* Ex: 2 */
+
+// var a = prompt("Enter Your Roll : ");
+// if((a == 10))
+// document.write("Name : Rahul");
+// else
+// document.write("Wrong Value");
+
+
+
+
+/* Else If Statement */
+
+// var result = 17;
+// if(result <= 30)
+// document.write("Fail");
+// else if (result <= 40)
+// document.write("Pass");
+// else if (result <= 60)
+// document.write("Good");
+// else
+// document.write("Very Good");
+
+
+/* Ex: 2 */
+
+// var result = prompt();
+// if(result <= 30) 
+// document.write("Fail");
+// else if (result <= 40)
+// document.write("Pass");
+// else if (result <= 60)
+// document.write("Good");
+// else
+// document.write("Very Good");
+
+
+
+
+/* Switch Statement */
+
+// var Day = 1;
+// switch(Day){
+//     case 1 :
+//     document.write("Sun");
+//     break;
+//     case 2 :
+//     document.write("Mon");
+//     break;
+//     case 3 :
+//     document.write("Tue");
+//     break;
+//     case 4 :
+//     document.write("Wed");
+//     break;
+//     case 5 :
+//     document.write("Thu");
+//     break;
+//     default:
+//     document.write("Wrong")
+// }
+
+
+/* Ex: 2 */
+
+// var Day = prompt();
+// switch(Day){
+//     case "Mon" :
+//     document.write("Working Day");
+//     break;
+//     case "Sat" :
+//     case "Sun" :
+//     document.write("Holiday");
+//     break;
+//     default:
+//     document.write("Wrong")
+// }
+
+
+
+
+/* For Loop */
+
+// for(i=0; i<5; i++)
+// {
+//     document.write(i + "<br>");
+// }
+// document.write( "<br>" + "Rest of the Code");
+
+
+/* Ex: 2 */
+
+// var i = 0;
+// for( ; i<5 ; i++)
+// {
+//     document.write(i + "<br>");
+// }
+// document.write( "<br>" + "Rest of the Code");
